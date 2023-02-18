fd5aa0c129bd214cc8042afc1c6d7ec0b4895b75
Higa <codehiga@gmail.com>
2023-02-18 00:34:38 -0300

    test: repository save method in persist new flash card usecase

--- src/domain/usecases/persist-new-flash-card.spec.ts
+++ src/domain/usecases/persist-new-flash-card.spec.ts
@@ -1,13 +1,30 @@
+import { InMemoryFlashCardRepository } from "./../../data/tests/memory-flash-card-repository";
 import { PersistNewFlashCard } from "./persist-new-flash-card";
 import { FlashCard } from "./../entities/flash-card";
 import { MissingFieldError } from "./../entities/errors/missing-field-error";
 import { ErrorResponse } from "./../../shared/error-response";
 import { FlashCardModel } from "../entities/models/flash-card-model";
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, vitest } from "vitest";
+import { FlashCardRepository } from "../../data/protocols/flash-card-repository";
+import { UnexpectedServerError } from "../../shared/error/unexpected-server-error";
+
+type sutTypes = {
+  sut: PersistNewFlashCard;
+  repository: FlashCardRepository;
+};
+
+const makeSut = (): sutTypes => {
+  const repository: FlashCardRepository = new InMemoryFlashCardRepository();
+  const sut = new PersistNewFlashCard(repository);
+  return {
+    sut,
+    repository,
+  };
+};
 
 describe("PersistNewFlashCard Usecase", () => {
   it("should not be able to create a new flash card if entity validation fails", async () => {
-    const sut = new PersistNewFlashCard();
+    const { sut } = makeSut();
     const invalidCreateFlashCardModel: FlashCardModel.Create = {
       awnser: "",
       category: "valid category",
@@ -21,8 +38,27 @@ describe("PersistNewFlashCard Usecase", () => {
     expect(error.msg).toEqual(new MissingFieldError("awnser").message);
   });
 
+  it("should not be able to create a new flash card if repository fails", async () => {
+    const { sut, repository } = makeSut();
+    const validCreateFlashCardModel: FlashCardModel.Create = {
+      awnser: "valid awnser",
+      category: "valid category",
+      dificulty: 1,
+      question: "valid question",
+      type: "valid type",
+    };
+    vitest.spyOn(repository, "save").mockRejectedValue(null);
+    const error = (await sut.execute(validCreateFlashCardModel))
+      .value as ErrorResponse;
+    expect(error.error).toBeInstanceOf(UnexpectedServerError);
+    expect(error.msg).toEqual(
+      new UnexpectedServerError("PersistNewFlashCard > flashCardRepo.save")
+        .message
+    );
+  });
+
   it("should be able to create a new flash card", async () => {
-    const sut = new PersistNewFlashCard();
+    const { sut } = makeSut();
     const validCreateFlashCardModel: FlashCardModel.Create = {
       awnser: "valid awnser",
       category: "valid category",
