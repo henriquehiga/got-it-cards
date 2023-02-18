1ab7b46dbf75178ae469a5faf06d70181d301cba
Higa <codehiga@gmail.com>
2023-02-18 00:34:33 -0300

    feat: repository save method in persist new flash card usecase

--- src/domain/usecases/persist-new-flash-card.ts
+++ src/domain/usecases/persist-new-flash-card.ts
@@ -1,10 +1,14 @@
+import { FlashCardRepository } from "./../../data/protocols/flash-card-repository";
 import { randomUUID } from "crypto";
 import { Either, left, right } from "../../shared/either";
 import { ErrorResponse } from "../../shared/error-response";
 import { FlashCardModel } from "../entities/models/flash-card-model";
 import { FlashCard } from "../entities/flash-card";
+import { UnexpectedServerError } from "../../shared/error/unexpected-server-error";
 
 export class PersistNewFlashCard {
+  constructor(private readonly flashCardRepo: FlashCardRepository) {}
+
   async execute(
     data: FlashCardModel.Create
   ): Promise<Either<ErrorResponse, FlashCard>> {
@@ -16,6 +20,16 @@ export class PersistNewFlashCard {
     const flashCardOrError = FlashCard.create(dataWithId);
     if (flashCardOrError.isLeft()) {
       return left(flashCardOrError.value);
+    }
+    try {
+      await this.flashCardRepo.save(flashCardOrError.value.props);
+    } catch (error: Error | any) {
+      return left({
+        error: new UnexpectedServerError(""),
+        msg: new UnexpectedServerError(
+          "PersistNewFlashCard > flashCardRepo.save"
+        ).message,
+      });
     }
     return right(flashCardOrError.value);
   }
