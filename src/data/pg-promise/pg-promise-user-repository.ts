9c3ecebaf948f838e5936a51b7139200882b9147
Higa <codehiga@gmail.com>
2023-02-19 16:16:04 -0300

    feat: user repository impl pg-promise

---
+++ src/data/pg-promise/pg-promise-user-repository.ts
@@ -0,0 +1,19 @@
+import { UserModel } from "../../domain/entities/models/user-model";
+import { db } from "../../infra/pg-promise";
+import { UserRepository } from "../protocols/user-repository";
+
+enum UserRepositoryQueries {
+  SAVE = "INSERT INTO gotitcards.users(id, name, email, password) VALUES (${id}, ${name}, ${email}, ${password});",
+}
+
+export class PgPromiseUserRepository implements UserRepository {
+  async save(data: UserModel.Model): Promise<UserModel.Model> {
+    try {
+      await db.connect();
+      await db.none(UserRepositoryQueries.SAVE, data);
+      return data;
+    } catch (error: any) {
+      throw new Error(error.toString());
+    }
+  }
+}
