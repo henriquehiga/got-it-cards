1049d990174d3efbe9b1c54d53c9b9e11038dae0
Higa <codehiga@gmail.com>
2023-02-19 18:24:24 -0300

    refactor: app listen on app.ts

--- src/main/app.ts
+++ src/main/app.ts
@@ -1,2 +1,9 @@
 import { setupApp } from "./configs/setup-app";
-setupApp();
+import { ENV_PORT } from "./configs/setup-env";
+
+(async () => {
+  const app = await setupApp();
+  app.listen(ENV_PORT, () =>
+    console.log("Server running on port: " + ENV_PORT)
+  );
+})();
--- src/main/configs/setup-app.ts
+++ src/main/configs/setup-app.ts
@@ -1,14 +1,10 @@
 import express, { Express } from "express";
 import { setupMiddlewares } from "./setup-middlewares";
-import { ENV_PORT } from "./setup-env";
 import setupRoutes from "./setup-routes";
 
 export const setupApp = async (): Promise<Express> => {
   const app = express();
   setupMiddlewares(app);
   setupRoutes(app);
-  app.listen(ENV_PORT, () =>
-    console.log("Server running on port: " + ENV_PORT)
-  );
   return app;
 };
