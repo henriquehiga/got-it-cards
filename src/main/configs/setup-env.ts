3d5e7acfa525d1005eb1e90a9d924802dbfe75df
Higa <codehiga@gmail.com>
2023-02-19 18:15:27 -0300

    feat: crypto key env

--- src/main/configs/setup-env.ts
+++ src/main/configs/setup-env.ts
@@ -6,3 +6,4 @@ export const ENV_DB_PORT = parseInt(process.env.DB_PORT ?? "5432");
 export const ENV_DB_NAME = process.env.DB_NAME;
 export const ENV_DB_USER = process.env.DB_USER;
 export const ENV_DB_PASS = process.env.DB_PASS;
+export const ENV_ENCRYPT_KEY = process.env.ENCRYPT_KEY ?? "key";
