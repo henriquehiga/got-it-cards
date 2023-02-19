7fd834c3e2899cdd34326af92b14a34b63e447b3
Higa <codehiga@gmail.com>
2023-02-19 18:47:55 -0300

    feat: find user by email

--- src/data/pg-promise/pg-promise-user-repository.ts
+++ src/data/pg-promise/pg-promise-user-repository.ts
@@ -4,9 +4,23 @@ import { UserRepository } from "../protocols/user-repository";
 
 enum UserRepositoryQueries {
   SAVE = "INSERT INTO gotitcards.users(name, email, password) VALUES (${name}, ${email}, ${password});",
+  FIND_BY_EMAIL = "SELECT TOP 1 FROM gotitcards.users WHERE email = $email",
 }
 
 export class PgPromiseUserRepository implements UserRepository {
+  async findByEmail(data: string): Promise<UserModel.Model | null> {
+    try {
+      await db.connect();
+      const founded = await db.one<UserModel.Model>(
+        UserRepositoryQueries.FIND_BY_EMAIL,
+        data
+      );
+      return founded;
+    } catch (error: any) {
+      throw new Error(error.toString());
+    }
+  }
+
   async save(data: UserModel.Model): Promise<UserModel.Model> {
     try {
       await db.connect();
