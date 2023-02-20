75a86f7651e314e1134f3d6131937e2748e09d04
Higa <codehiga@gmail.com>
2023-02-20 20:07:29 -0300

    feat: FIND_BY_USER_ID on pg-flash-card-repo

--- src/data/pg-promise/pg-promise-flash-card-repository.ts
+++ src/data/pg-promise/pg-promise-flash-card-repository.ts
@@ -4,9 +4,23 @@ import { FlashCardRepository } from "./../protocols/flash-card-repository";
 
 enum FlashCardRepositoryQueries {
   SAVE = "INSERT INTO gotitcards.flash_cards(id, user_id, question, awnser, category, dificulty, type) VALUES (${id}, ${user_id}, ${question}, ${awnser}, ${category}, ${dificulty}, ${type});",
+  FIND_BY_USER_ID = "SELECT * FROM gotitcards.flash_cards WHERE user_id = $1",
 }
 
 export class PgPromiseFlashCardRepository implements FlashCardRepository {
+  async findByUserId(data: string): Promise<FlashCardModel.Model[]> {
+    try {
+      await db.connect();
+      const flashCards = await db.manyOrNone<FlashCardModel.Model>(
+        FlashCardRepositoryQueries.FIND_BY_USER_ID,
+        data
+      );
+      return flashCards ?? [];
+    } catch (error: any) {
+      throw new Error(error.toString());
+    }
+  }
+
   async save(data: FlashCardModel.Model): Promise<FlashCardModel.Model> {
     try {
       await db.connect();
