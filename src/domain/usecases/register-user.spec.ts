e540bf301cf79c95c6520f16ad24ad6af4eb4714
Higa <codehiga@gmail.com>
2023-02-23 21:55:35 -0300

    test: user already exists

--- src/domain/usecases/register-user.spec.ts
+++ src/domain/usecases/register-user.spec.ts
@@ -1,9 +1,10 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, vitest } from "vitest";
 import { UserRepository } from "../../data/protocols/user-repository";
 import { UserModel } from "../entities/models/user-model";
 import { InMemoryUserRepository } from "./../../data/tests/memory-user-repository";
 import { ErrorResponse } from "./../../shared/error-response";
 import { MissingFieldError } from "./../entities/errors/missing-field-error";
+import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
 import { RegisterUser } from "./register-user";
 
 type sutTypes = {
@@ -32,5 +33,25 @@ describe("RegisterUser Usecase", () => {
       .value as ErrorResponse;
     expect(error.error).toBeInstanceOf(MissingFieldError);
     expect(error.msg).toEqual(new MissingFieldError("name").message);
+  });
+
+  it("should not be able to create a new user if email provided already exists", async () => {
+    const { sut, repository } = makeSut();
+    const validCreateUserModel: UserModel.Create = {
+      name: "valid name",
+      email: "valid@mail.com",
+      password: "valid_hashed_password",
+    };
+    vitest.spyOn(repository, "findByEmail").mockReturnValue(
+      Promise.resolve({
+        email: "valid@mail.com",
+        last_login: "valid_last",
+        name: "valid name",
+        password: "valid_password",
+      })
+    );
+    const error = (await sut.execute(validCreateUserModel))
+      .value as ErrorResponse;
+    expect(error.error).toBeInstanceOf(UserAlreadyExistsError);
   });
 });
