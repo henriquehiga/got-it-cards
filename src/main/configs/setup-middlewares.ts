6d13b6cbdbb801cd0740f85f79e40614cb2f8425
Higa <codehiga@gmail.com>
2023-02-25 03:02:59 -0300

    feat: add cors origin front

--- src/main/configs/setup-middlewares.ts
+++ src/main/configs/setup-middlewares.ts
@@ -4,12 +4,12 @@ import validateTokenMiddleware from "./validate-token-middleware";
 
 export const setupMiddlewares = (app: Express.Application) => {
   app.use(json());
-  app.use(
-    cors({
-      origin: "https://got-it-cards-front.vercel.app",
-    })
-  );
+  app.use(cors());
   app.use((req, res, next) => {
+    res.set(
+      "Access-Control-Allow-Origin",
+      "https://got-it-cards-front.vercel.app/"
+    );
     validateTokenMiddleware(req, res, next);
   });
   return app;
