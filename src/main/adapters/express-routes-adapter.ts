7c716943d92a2783ad044e3874dd02ad4cfc898b
Higa <codehiga@gmail.com>
2023-02-25 03:11:19 -0300

    feat: add res allow origin on adapter

--- src/main/adapters/express-routes-adapter.ts
+++ src/main/adapters/express-routes-adapter.ts
@@ -3,6 +3,10 @@ import { Controller } from "../../presentations/protocols/controller";
 
 export const adaptRoute = (controller: Controller) => {
   return async (req: Request, res: Response) => {
+    res.set(
+      "Access-Control-Allow-Origin",
+      "https://got-it-cards-front.vercel.app"
+    );
     const httpResponse = await controller.handle(req);
     res.status(httpResponse.statusCode).json(httpResponse);
   };
