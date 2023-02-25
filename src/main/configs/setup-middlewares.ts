5d59564f8143cdad32389a3e7e222edc5c1eb4dc
Higa <codehiga@gmail.com>
2023-02-25 03:00:36 -0300

    fix: token validate

--- src/main/configs/setup-middlewares.ts
+++ src/main/configs/setup-middlewares.ts
@@ -1,10 +1,14 @@
-import Express, { json } from "express";
 import cors from "cors";
+import Express, { json } from "express";
 import validateTokenMiddleware from "./validate-token-middleware";
 
 export const setupMiddlewares = (app: Express.Application) => {
   app.use(json());
-  app.use(cors());
+  app.use(
+    cors({
+      origin: "https://got-it-cards-front.vercel.app",
+    })
+  );
   app.use((req, res, next) => {
     validateTokenMiddleware(req, res, next);
   });
--- src/presentations/controllers/validate-token-controller.ts
+++ src/presentations/controllers/validate-token-controller.ts
@@ -6,10 +6,15 @@ import { OK, REQUEST_BODY_ERROR } from "../protocols/http-errors";
 export class ValidateTokenController implements Controller {
   async handle(request: HttpRequest): Promise<HttpResponse> {
     const token = request.body.token;
+    let tokenJsonValidate;
     if (!token) {
       return REQUEST_BODY_ERROR("Obrigatório fornecer um TOKEN");
     }
-    const tokenJsonValidate = JwtToken.validate(token);
+    try {
+      tokenJsonValidate = JwtToken.validate(token);
+    } catch (error: any) {
+      return REQUEST_BODY_ERROR("Obrigatório fornecer um TOKEN VÁLIDO");
+    }
     return OK(tokenJsonValidate);
   }
 }
