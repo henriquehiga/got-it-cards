418f950b7b904411086b8ae8b6d179663152408c
Higa <codehiga@gmail.com>
2023-02-26 10:55:32 -0300

    fix: array length add cards

--- src/domain/usecases/build-deck-flash-cards.ts
+++ src/domain/usecases/build-deck-flash-cards.ts
@@ -14,15 +14,12 @@ export class BuildDeckFlashCards {
   ): Promise<Either<ErrorResponse, FlashCardModel.Model[]>> {
     const flashCards = await this.getFlashCardsByCategory.execute(category);
     const deckToReturn: FlashCardModel.Model[] = [];
-
     if (flashCards.isLeft()) {
       return left(flashCards.value);
     }
-
     const easyFlashCardsDeck: FlashCardModel.Model[] = [];
     const normalFlashCardsDeck: FlashCardModel.Model[] = [];
     const hardFlashCardsDeck: FlashCardModel.Model[] = [];
-
     flashCards.value.forEach((flashCard) => {
       if (flashCard.dificulty == 1) {
         easyFlashCardsDeck.push(flashCard);
@@ -32,33 +29,26 @@ export class BuildDeckFlashCards {
         hardFlashCardsDeck.push(flashCard);
       }
     });
-
     const minCards = 6;
     const maxCards = 10;
-
     const quantity =
       flashCards.value.length >= maxCards ? maxCards : flashCards.value.length;
-
     const easyFlashCardsMax = Math.floor(quantity * 0.2);
     const normalFlashCardsMax = Math.floor(quantity * 0.3);
     const hardFlashCardsMax = Math.floor(quantity * 0.5);
-
     function selectRandomCards(
       array: FlashCardModel.Model[],
       quantity: number
     ) {
       const cards = [];
-
       for (let i = 0; i < quantity; i++) {
-        cards.push(array[randomInt(0, array.length)]);
+        cards.push(array[randomInt(0, array.length - 1)]);
       }
       deckToReturn.push(...cards);
     }
-
     selectRandomCards(easyFlashCardsDeck, easyFlashCardsMax);
     selectRandomCards(normalFlashCardsDeck, normalFlashCardsMax);
     selectRandomCards(hardFlashCardsDeck, hardFlashCardsMax);
-
     if (deckToReturn.length < minCards) {
       const concatDeck = [
         ...easyFlashCardsDeck,
@@ -67,7 +57,6 @@ export class BuildDeckFlashCards {
       ];
       return right(concatDeck);
     }
-
     return right(deckToReturn);
   }
 }
