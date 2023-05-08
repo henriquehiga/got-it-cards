3acfcecaade4b45cbda4e1c6a9405f6aa4ed7b90
Henrique Higa <codehiga@gmail.com>
2023-05-08 01:50:30 -0300

    chore: ignore test

--- src/main/configs/setup-env.ts
+++ src/main/configs/setup-env.ts
@@ -1,6 +1,6 @@
 import { config } from "dotenv";
 config();
-export const ENV_PORT = process.env.PORT;
+export const ENV_PORT = process.env.PORT ?? 4501;
 export const ENV_DB_HOST = process.env.DB_HOST;
 export const ENV_DB_PORT = parseInt(process.env.DB_PORT ?? "5432");
 export const ENV_DB_NAME = process.env.DB_NAME;
